e58bc32006e9deb8dcb19350bb0888d0f7f5afe2
Eugenie Chupris <zhenuh15@gmail.com>
2020-09-04 08:27:07 +0300

    add map

--- booking/src/app/main-page-content/main-page-content.component.ts
+++ booking/src/app/main-page-content/main-page-content.component.ts
@@ -7,9 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainPageContentComponent implements OnInit {
 
-  constructor() { }
+  constructor() { 
+  }
 
+  
   ngOnInit(): void {
+    var mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
+    var MapboxGeocoder = require('@mapbox/mapbox-gl-geocoder');
+    
+ 
+    mapboxgl.accessToken = 'pk.eyJ1IjoiY2h1cGVycyIsImEiOiJjazNqcjJ4YnQwM3l5M2xwOXppNmtkMWF4In0.MqIEuzBBpryI6_dps113lw';
+    var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/chupers/ck3n2w1mv1sbc1cpaqf1vbkof',
+    });
+    var geocoder = new MapboxGeocoder({
+      accessToken: mapboxgl.accessToken,
+      mapboxgl: mapboxgl
+      });
   }
 
 }
